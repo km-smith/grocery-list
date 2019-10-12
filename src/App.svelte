12fbedd69055449923769de5922161b48faab737
Kyle Smith <kysmith@kysmith-mn3.linkedin.biz>
2019-10-11 21:24:18 -0700

    Add basic firebase authentication setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,30 @@
 <script>
-	export let name;
+  import firebase from 'firebase/app';
+  import 'firebase/auth';
+
+  const initialization = (async function() {
+    const response = await fetch('/__/firebase/init.json');
+    firebase.initializeApp(await response.json());
+  }());
+
+  (async function() {
+    await initialization;
+    const result = await firebase.auth().getRedirectResult();
+    console.log(result);
+  }());
+
+  function handleSignInClick() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('email');
+    firebase.auth().signInWithRedirect(provider);
+  }
 </script>
 
-<style>
-	h1 {
-		color: red;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+{#await initialization}
+	<!-- promise is pending -->
+	<p>waiting for the promise to resolve...</p>
+{:then value}
+	<button on:click={handleSignInClick}>Sign In</button>
+{:catch error}
+	<p>Something went wrong: {error.message}</p>
+{/await}
