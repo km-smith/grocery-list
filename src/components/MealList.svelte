8d26f57e223fe3f297562f36c29a30fdefe38fa8
Kyle Smith <kysmith@kysmith-mn3.linkedin.biz>
2019-11-09 20:57:05 -0800

    Add firestore setup

--- src/components/MealList.svelte
+++ src/components/MealList.svelte
@@ -1,1 +1,20 @@
+<script>
+  import { firebase } from '../utils/firebase';
+  const db = firebase.firestore();
+
+  let meals = [];
+
+  (async function() {
+    const querySnapshot = await db.collection('meals').get();
+    meals = querySnapshot.docs.map(doc => {
+      return { ...doc.data(), id: doc.id };
+    });
+  }());
+</script>
+
 <h1>Meal List</h1>
+<ul>
+  {#each meals as meal}
+		<li>{meal.id}: {meal.name}</li>
+	{/each}
+</ul>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,6 @@
 import fb from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 export const firebase = fb;
 
